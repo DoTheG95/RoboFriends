c5ad19f554b925dbaff31e1d4c568e3614a91af6
DoTheG95 <104708516+DoTheG95@users.noreply.github.com>
2023-05-31 18:58:17 +1200

    Removed Robots.js file and fetching data from json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from "react";
 import CardList from "./CardList";
-import { robots } from "./Robots";
 import SearchBox from "./SearchBox"
 import './App.css'
 
@@ -14,7 +13,9 @@ class App extends Component {
     }
 
     componentDidMount(){
-        this.setState({ robots: robots });
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({robots: users}))
     }
 
     onSearchChange = (event) => {
