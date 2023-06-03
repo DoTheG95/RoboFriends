bb073eb78e34ea3fe0816559598196d3cba1cfa5
DoTheG95 <104708516+DoTheG95@users.noreply.github.com>
2023-06-03 23:02:08 +1200

    Added scrolling feature to the page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox"
+import Scroll from './Scroll';
 import './App.css'
 
 class App extends Component {
@@ -15,7 +16,7 @@ class App extends Component {
     componentDidMount(){
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState({robots: users}))
+            .then(users => {this.setState({robots: users})})
     }
 
     onSearchChange = (event) => {
@@ -26,14 +27,19 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robot =>{
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
-
+        if (this.state.robots.length === 0){
+            return <h1>Loading</h1>
+        } else{
         return (
             <div className="tc">
                 <h1 className="f2">RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
+                <Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll>
             </div>);
         }
+    }
 }
 
 export default App;
